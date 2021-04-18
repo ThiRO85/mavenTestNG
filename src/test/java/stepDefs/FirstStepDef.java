6c87c946004a008b2ee777a5587295f2eda87e68
ThiRO85 <thgr85@gmail.com>
2021-04-17 21:08:53 -0300

    TestNG with Maven and Cucumber - Minimum to pass a test

---
+++ src/test/java/stepDefs/FirstStepDef.java
@@ -0,0 +1,36 @@
+package stepDefs;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstStepDef {
+
+    //WebDriver driver;
+
+    @Given("Launch the browser")
+    public void launchTheBrowser() {
+        //WebDriverManager.chromedriver().setup();
+        //driver = new ChromeDriver();
+        System.out.println("Launch the browser");
+    }
+
+    @When("Hit Google on your browser")
+    public void hitGoogleOnYourBrowser() {
+        System.out.println("Hit Google on your browser");
+    }
+
+    @Then("Enter {string} in the search text box")
+    public void enterInTheSearchTextBox(String arg0) {
+        System.out.println("Enter in the search text box");
+    }
+
+    @And("Select the first result")
+    public void selectTheFirstResult() {
+        System.out.println("Select the first result");
+    }
+}
